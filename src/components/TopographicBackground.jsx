bf75ace09e098cac2c7a8bc9b39ba2c9202a80ad
alaricjames <alaricjames@hotmail.co.uk>
2025-02-09 15:08:10 +0000

    try again

--- src/components/TopographicBackground.jsx
+++ src/components/TopographicBackground.jsx
@@ -12,12 +12,15 @@ const TopographicBackground = () => {
         {/* Base background */}
         <path 
           d="M0 0 C633.6 0 1267.2 0 1920 0 C1920 362.01 1920 724.02 1920 1097 C1286.4 1097 652.8 1097 0 1097 C0 734.99 0 372.98 0 0 Z" 
-          fill="#2F3C7E" // Your site's primary blue color
+          fill="#2F3C7E" // Main website blue
         />
         
         {/* Contour paths */}
-        <g opacity="0.15" fill="#FBEAEB"> {/* Your site's light pink color */}
-          {/* SVG paths */}
+        <g opacity="0.15">
+          <path d="M0 0 C286.77 0 573.54 0 869 0..." fill="#2F3C7E" transform="translate(798,321)"/>
+          <path d="M0 0 C1.95418308 2.93127462..." fill="#2F3C7E" transform="translate(386,300)"/>
+          <path d="M0 0 C1.125 1.625 1.125 1.625..." fill="#2F3C7E" transform="translate(1031,238)"/>
+          {/* Add remaining paths with the same fill color */}
         </g>
       </svg>
     </div>
