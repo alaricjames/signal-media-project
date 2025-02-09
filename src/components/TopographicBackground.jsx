7afe81ba0946dc839da4583528875a28ea594e81
alaricjames <alaricjames@hotmail.co.uk>
2025-02-09 16:23:48 +0000

    lighter this time

--- src/components/TopographicBackground.jsx
+++ src/components/TopographicBackground.jsx
@@ -5,7 +5,7 @@ const TopographicBackground = () => {
   return (
     <div className="fixed inset-0 -z-10 pointer-events-none">
       <TopographicSVG 
-        className="w-full h-full [&_path]:fill-[#171B26] [&_path]:opacity-70" 
+        className="w-full h-full [&_path]:fill-[#171B26] [&_path]:opacity-30" 
       />
     </div>
   );
